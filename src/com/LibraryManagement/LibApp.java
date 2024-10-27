33155ee44956c3db1e57966a12bcc5f367e87d8f
M-Kiran534 <kiranachari505@gmail.com>
2024-10-27 21:40:09 +0530

    Add files via upload

---
+++ src/com/LibraryManagement/LibApp.java
@@ -0,0 +1,25 @@
+package com.LibraryManagement;
+
+//import java.util.Scanner;
+
+public class LibApp {
+	public static void main(String[] args) {
+		
+		Library l = new Library();
+		l.start();
+//		Book b1 = new Book(1,"the life","AAA","science");
+//		Book b2 = new Book(2,"the goat life","BBB","history");
+//		Book b3 = new Book(3,"the life of pie","HHH","science");
+//		Book b4 = new Book(4,"the bhahubali","RRR","history");
+//		
+//		l.addBook(b1);
+//		l.addBook(b2);
+//		l.addBook(b3);
+//		l.addBook(b4);
+//		
+//		Scanner s = new Scanner(System.in);
+//		String title = "the life";
+//		System.out.println(l.findBook(title)!=null);
+		
+	}
+}
